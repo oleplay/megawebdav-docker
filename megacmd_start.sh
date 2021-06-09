fe6d5a1f82ec8ac0394ff2035452d8ba81f77385
ShobuPrime <anthony.dardano@outlook.com>
2021-06-09 18:51:52 -0400

    Update megacmd_start.sh

--- megacmd_start.sh
+++ megacmd_start.sh
@@ -1,7 +1,7 @@
 #1/bin/bash
 
 # Attempting to use docker environment variables
-groupadd -g ${PGID} megausers && useradd -u ${PUID} megacmd && usermod -g megausers megacmd && usermod -G root megacmd
+groupadd -g ${PGID} megausers && usermod -g megausers megacmd && usermod -G root megacmd
 
 mega-version
 echo "------------------------------------------------------------------------------"
