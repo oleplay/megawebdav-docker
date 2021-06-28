2fe551dabc6f431816461caf59e2025901c02c01
ShobuPrime <anthony.dardano@outlook.com>
2021-06-28 12:36:10 -0400

    Update megacmd_start.sh

--- megacmd_start.sh
+++ megacmd_start.sh
@@ -3,13 +3,13 @@
 # Attempting to use docker environment variables
 groupmod -g ${PGID} ${GROUP} && usermod -u ${PUID} ${USER} && usermod -g ${PGID} ${USER}
 
-mega-cmd-server --debug --skip-lock-check
+sudo -u ${USER} mega-cmd-server --debug --skip-lock-check
 
-mega-version
+sudo -u ${USER} mega-version
 echo "------------------------------------------------------------------------------"
-mega-whoami
+sudo -u ${USER} mega-whoami
 echo "------------------------------------------------------------------------------"
-mega-https on
-mega-https
+sudo -u ${USER} mega-https on
+sudo -u ${USER} mega-https
 echo "------------------------------------------------------------------------------"
 #mega-sync ~/Mega /
