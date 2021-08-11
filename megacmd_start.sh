01b2daf88baebd6b6e34c31ccaf4bab5fd828a4f
ShobuPrime <anthony.dardano@outlook.com>
2021-08-11 15:20:57 -0400

    Update megacmd_start.sh

--- megacmd_start.sh
+++ megacmd_start.sh
@@ -6,10 +6,11 @@ groupmod -g ${PGID} ${GROUP} && usermod -u ${PUID} ${USER} && usermod -g ${PGID}
 sudo -u ${USER} mega-cmd-server --debug --skip-lock-check
 
 sudo -u ${USER} mega-version
+sudo -u ${USER} mega-login ${SESSION_ID}
 echo "------------------------------------------------------------------------------"
 sudo -u ${USER} mega-whoami
 echo "------------------------------------------------------------------------------"
 sudo -u ${USER} mega-https on
 sudo -u ${USER} mega-https
 echo "------------------------------------------------------------------------------"
-#mega-sync ~/Mega /
+sudo -u ${USER} mega-sync ~/Mega /
