207e955f5522e0c68ae90f99d25483af78caa6f7
ShobuPrime <anthony.dardano@outlook.com>
2021-09-05 22:20:49 -0400

    Update megacmd_start.sh

--- megacmd_start.sh
+++ megacmd_start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Attempting to use docker environment variables
-#groupmod -g ${PGID} ${GROUP} && usermod -u ${PUID} ${USER} && usermod -g ${PGID} ${USER}
-
 echo "Setting PID and GID environmental variables"
 ./fix_permissions.sh
 
@@ -11,20 +9,23 @@ mega-cmd-server --debug --skip-lock-check &>/dev/null &
 
 su -c mega-version ${USER}
 existing_session=/home/${USER}/.megaCmd/session
-if [ -f  "$existing_session"]; then
+if [ -f  "$existing_session" ]; then
     echo "Session file found, who am I?"
-    #su -c mega-login ${USER}
+    su -c mega-whoami ${USER}
+    echo "------------------------------------------------------------------------------"
+    echo "What sync tasks what I running?"
+    su -c mega-sync ${USER}
 else 
     echo "No detected session. Logging in as:"
     su -c mega-login ${SESSION_ID} ${USER}
+    su -c mega-whoami ${USER}
+    echo "------------------------------------------------------------------------------"
+    su -c mega-https on ${USER}
+    echo "------------------------------------------------------------------------------"
+    su -c mega-https ${USER}
+    echo "------------------------------------------------------------------------------"
+    su -c mega-sync /home/${USER}/Mega / ${USER}
 fi
-echo "------------------------------------------------------------------------------"
-su -c mega-whoami ${USER}
-echo "------------------------------------------------------------------------------"
-su -c mega-https on ${USER}
-su -c mega-https ${USER}
-echo "------------------------------------------------------------------------------"
-su -c mega-sync /home/${USER}/Mega / ${USER}
 
 # Cheap and dirty way to keep container running
 
