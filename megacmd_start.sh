a4497f8c62ac020813643741cc910a2eaffb6506
ShobuPrime <anthony.dardano@outlook.com>
2021-09-05 21:53:56 -0400

    Update megacmd_start.sh

--- megacmd_start.sh
+++ megacmd_start.sh
@@ -8,9 +8,10 @@ mega-cmd-server --debug --skip-lock-check &>/dev/null &
 su -c mega-version ${USER}
 existing_session=/home/${USER}/.megaCmd/session
 if [ -f  "$existing_session"]; then
-    su -c mega-login ${USER}
+    echo "Session file found, who am I?"
+    #su -c mega-login ${USER}
 else 
-    echo "Existing session found"
+    echo "No detected session. Logging in as:"
     su -c mega-login ${SESSION_ID} ${USER}
 fi
 echo "------------------------------------------------------------------------------"
